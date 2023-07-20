d7fe93b16d8264c22b8bb10dc6efb35a599a56fa
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-20 17:10:08 -0400

    Parse basic CSV file into names and zip codes

---
+++ lib/event_manager.rb
@@ -0,0 +1,29 @@
+require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode == nil
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
